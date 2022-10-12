6d54a4f5889713477b4120bb9268b52e2805b32e
Yauris12 <alexisaube28@gmail.com>
2022-10-12 14:09:17 -0500

    build git

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import About from './AboutPage'
+import Cart from './CartPage'
+import CheckOut from './CheckoutPage'
+import Error from './ErrorPage'
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Private from './PrivateRoute'
+export { About, Cart, CheckOut, Error, Home, Products, SingleProduct, Private }
